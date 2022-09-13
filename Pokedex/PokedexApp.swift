e8eb003a115c50e3d6d13fde2756be85244b03ee
Nihal-m24 <nihal.memon24@gmail.com>
2022-09-12 21:26:40 -0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Nihal Memon on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
